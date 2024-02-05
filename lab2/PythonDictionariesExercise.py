df033f7440dc0a75095a7a54529dbd0b72779900
Ali <Alizat17022006@gmail.com>
2024-02-05 19:49:48 +0600

    Let's go

---
+++ lab2/PythonDictionariesExercise.py
@@ -0,0 +1,25 @@
+#1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+#2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+
+#3
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+
+#4
